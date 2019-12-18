85925929643506eec87e14b16de05b7ae0a5bc78
sara khaled <sara.hrb1@hotmail.com>
2019-12-18 20:40:16 +0300

    firstComment

--- src/project_list/Right/RightSide.js
+++ src/project_list/Right/RightSide.js
@@ -1,53 +1,40 @@
 import React, { Component } from 'react'
 import {Image, Navbar, Nav,Container,Col,Button} from 'react-bootstrap'
 import axios from 'axios'
-
+import store from '../.././store'
 export default class RightSide extends Component {
-    
     applyhundler = () => {
-       const url = `${this.props.details._id}`+`/newsfreelancer`
+        const st = store.getState()
+        console.log(st.auth.user.id);
+       const url = `http://localhost:5001/projects/${this.props.details._id}/addfreelancer/${st.auth.user.id}`
+       //${this.props.details._id}/addfreelancer/${st.auth.user.id}
         axios({
             method: 'POST',
             url: url
           }).then(response => {
-            console.log('Hi')
-            console.log(response) // take a look at what you get back!
-            console.log('Hi')
-            this.setState({ current: response.data})
+            console.log(response);
           })
     }
-
-
     render() {
-
-        console.log("ptp");
-        console.log(this.props);
-        
-        
+        const st = store.getState()
+        console.log(st);
+        console.log(st.auth.user.id);
+        // console.log("ptp");
+        // console.log(this.props);
         let d = this.props.details.price > 0 ?
-
-        
         <div className="right-detail">
-                
-            
             <figure className="film-backdrop">
-                    
                     <h1>{this.props.details.title}</h1>
                     <h2>{this.props.details.description}</h2>
                     <img src={this.props.details.pic} /><br/>
                     <br/>
                     <br/>
-                    <center><Button > Apply ...</Button></center>
+                    <center><Button onClick={this.applyhundler}> Apply ...</Button></center>
             </figure>
-                          
         </div>          
-        
 : <div className="right-detail">
-                
                     No Project selected
-                
             </div>
-
         return (
             <div className="champs-detail">
                 <div className="right-details">
@@ -55,8 +42,8 @@ export default class RightSide extends Component {
         </div>
                 {d}
             </div>
-            
         )
     }
 }
+
 
